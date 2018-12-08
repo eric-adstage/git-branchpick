5d7da1683648b216057e2dc0bc2522817f8fd298
Eric Foster <eric@adstage.io>
2018-12-08 14:44:58 -0800

    Path tweak.

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ exec_path="$HOME/bin/git-branchpick"
 mkdir -p $exec_path
 cp bin/branchpick $exec_path
 chmod 755 $exec_path
-git config --global alias.branchpick !$exec_path/branchpick
+git config --global alias.bp !$exec_path/branchpick
